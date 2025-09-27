d99eacbfb781b1c3e1e678bd1117eb57a8069aeb
Arsalan Muteeb Mir <arsalanmir29@gmail.com>
2025-09-27 12:50:10 +0530

    Changes

--- reactproject1/src/main.jsx
+++ reactproject1/src/main.jsx
@@ -1,9 +1,31 @@
-import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div><h1>My App</h1></div>
+  )
+}
+// const ReactElem =  {// this myapp gets coverted like this
+//   type : 'a',
+//   props:  {
+//     href:'https://google.com',
+//     target : '_blank'
+//   },
+//   children: 'Click me to visit google'
+   
+// }
+const anotherelem =  (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+const reactElem =  React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'Click me to vist goolgul'
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  reactElem
+ 
 )
